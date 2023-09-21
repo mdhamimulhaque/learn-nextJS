3522ea592edd788b022cb5ebd4c846b67e7f2f4f
md Hamimul Haue <mdhamimulhaque@gmail.com>
2023-09-21 17:49:26 +0600

    next js custom error page

---
+++ src/pages/404.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <img
+        src="https://static.vecteezy.com/system/resources/previews/004/639/366/non_2x/error-404-not-found-text-design-vector.jpg"
+        alt=""
+        width={"100%"}
+      />
+    </div>
+  );
+};
+
+export default ErrorPage;
