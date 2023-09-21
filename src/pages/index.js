268fb9e0057a1817bc665635d5bfa1efcf1e4ee6
md Hamimul Haue <mdhamimulhaque@gmail.com>
2023-09-21 23:49:31 +0600

    basic layout setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import { Button } from "antd";
 import Link from "next/link";
+import RootLayout from "../../components/Layouts/RootLayout";
 
 const HomePage = () => {
   return (
@@ -13,3 +14,7 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
+HomePage.getLayout = function getLayout(page) {
+  return <RootLayout>{page}</RootLayout>;
+};
